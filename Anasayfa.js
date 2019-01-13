34e453f92c954aaba2cfe1ba32024ec07cbfae2b
burakhst53 <46582129+burakhst53@users.noreply.github.com>
2019-01-13 18:30:19 +0300

    ilk gönderme

---
+++ Anasayfa.js
@@ -0,0 +1,73 @@
+var arttir = 1;
+setInterval(function () {
+    degistir(arttir);
+    if (arttir == 3) {
+        arttir = 1;
+    } else {
+        arttir++;
+    }
+}, 3000);
+function degistir(counter) {
+    if (counter == 1) {
+        document.getElementById("reklam1").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam1.png'></a>";
+    } else if (counter == 2) {
+        document.getElementById("reklam1").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam2.png'></a>";
+    } else if (counter == 3) {
+        document.getElementById("reklam1").innerHTML = "<a href='https://www.pubg.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam3.jpg'></a>";
+    }
+}
+var arttirr = 1;
+setInterval(function () {
+    degistirr(arttirr);
+    if (arttirr == 3) {
+        arttirr = 1;
+    } else {
+        arttirr++;
+    }
+}, 3000);
+function degistirr(sayac) {
+    if (sayac == 1) {
+        document.getElementById("reklam2").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam1.png'></a>";
+    } else if (sayac == 2) {
+        document.getElementById("reklam2").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam2.png'></a>";
+    } else if (sayac == 3) {
+        document.getElementById("reklam2").innerHTML = "<a href='https://www.pubg.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam3.jpg'></a>";
+    }
+}
+var arttr = 1;
+setInterval(function () {
+    degistr(arttr);
+    if (arttr == 3) {
+        arttr = 1;
+    } else {
+        arttr++;
+    }
+}, 3000);
+function degistr(countr) {
+    if (countr == 1) {
+        document.getElementById("reklamBir").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam1.png'></a>";
+    } else if (countr == 2) {
+        document.getElementById("reklamBir").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam2.png'></a>";
+    } else if (countr == 3) {
+        document.getElementById("reklamBir").innerHTML = "<a href='https://www.pubg.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam3.jpg'></a>";
+    }
+}
+
+var artr = 1;
+setInterval(function () {
+    degistrr(artr);
+    if (artr == 3) {
+        artr = 1;
+    } else {
+        artr++;
+    }
+}, 3000);
+function degistrr(countrr) {
+    if (countrr == 1) {
+        document.getElementById("reklamIki").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam1.png'></a>";
+    } else if (countrr == 2) {
+        document.getElementById("reklamIki").innerHTML = "<a href='https://www.norsmt2.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam2.png'></a>";
+    } else if (countrr == 3) {
+        document.getElementById("reklamIki").innerHTML = "<a href='https://www.pubg.com/'><img src='file:///C:/Users/ENSAR%20HASTA/Desktop/Wike%20Up/Foto%C4%9Fraflar/reklam3.jpg'></a>";
+    }
+}
